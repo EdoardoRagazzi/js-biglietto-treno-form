38631414c13159430edf8e35dc44747708e7d585
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-06-04 16:18:34 +0200

    Submit Value Data

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Javascript
+// VARIABILE CHE RACCHIUDE LA FUNCTION SUBMIT LA QUALE A SUA VOLTA RACCHIUDE VARI CALCOLI
+var submit = document.getElementById('submit');
+submit.addEventListener("click", function () {
+
+    // Take the name 
+    var value = document.getElementById('name').value;
+    console.log(value);
+    // Take the Km 
+    var km = document.getElementById('km').value;
+    console.log(km);
+    //  Take the Range Age
+    var rangeAge = document.getElementById('range-age').value;
+    console.log(rangeAge);
+
+});
